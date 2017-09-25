c6580e1f35089d55de26d04c8087c9b80f348d05
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 02:06:30 +0000

    automatically backed up by learn

---
+++ lib/hello.rb
@@ -0,0 +1,14 @@
+def hello_t(names)
+  # if block_given?
+  #   counter = 0
+  #   
+  # else
+  #   puts "No block was given"
+  # end
+end
+
+# hello_t(names) do |name|
+#   if name.start_with("T")
+#     puts "hello #{name}"
+#   end
+# end
